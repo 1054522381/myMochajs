c53ed874125b135f5befec4fbc0866514048f161
Elma Guo <elma.guo@ehealth.com>
2019-11-27 16:38:02 +0800

    mocha browser

--- tests.js
+++ tests.js
@@ -1,23 +1,17 @@
-/*
-define(function(require, exports, module) {
-	var expect = require('node_modules/chai/chai.js').expect;
-	return describe('test add',function(){
-			it('1+1=2',function(){
-					console.log("==============");
-					expect(add(1,1)).to.be.equal(2);
-			});
-	});
-});*/
+const expect = chai.expect;
+console.log("=====tests=========");
 
-require(['config/require.config'], function() {
-	require(['node_modules/chai/chai.js'], function(chai) {
-        var expect = chai.expect;
-		describe('test add',function(){
-			it('1+1=2',function(){
-					console.log("==============");
-					expect(add(1,1)).to.be.equal(2);
-			});
-		});
-    });
-	
+before(function() {
+	console.log("======before========");
+});
+
+after(function() {
+	console.log("======after========");
+});
+
+describe('test add',function(){
+	it('1+1=2',function(){
+			console.log("======it========");
+			expect(add(1,1)).to.be.equal(2);
+	});
 });
